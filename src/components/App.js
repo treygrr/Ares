9b028ddc256310378d567daa2dfae7c5c8b25533
trey <trey@lavidadev.com>
2019-11-03 08:28:51 -0500

    Add housekeeping

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
-
 import Maps from './Maps';
-import Request from './Request';
+import DataList from './DataList'
 
-let Re = new Request();
-
-Re.getRequest();
-
-class App extends React.Component {
-    render () {
-        return (
-            <main className="wrapper flex flex-row position-absolute">
-                <section className="three shadow position-relative z-front">
-                    
-                </section>
-                <section className="seven position-relative z-back">
-                    <Maps></Maps>
-                </section>
-            </main>
-            )
-    }
+const App = () => {
+    return (
+        <main className="wrapper flex flex-row position-absolute">
+            <section className="three shadow position-relative z-front">
+                <DataList></DataList>
+            </section>
+            <section className="seven position-relative z-back">
+                <Maps></Maps>
+            </section>
+        </main>
+    );
 };
 
 export default App;
