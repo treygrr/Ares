9239bf87f6884219c3146dfc4091f127687f40c9
trey <trey@lavidadev.com>
2019-11-03 04:55:08 -0500

    Add html and styling for app page

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import Maps from './Maps'
 
-const App = () => {
-    return (
-        <main class="wrapper flex flex-row position-absolute">
-            <section class="three shadow position-relative z-front">
-                test
-            </section>
-            <section class="seven position-relative z-back">
-                <Maps></Maps>
-            </section>
-        </main>
-    )
+
+import Maps from './Maps';
+import Request from './Request';
+
+let Re = new Request();
+
+Re.getRequest();
+
+class App extends React.Component {
+    render () {
+        return (
+            <main className="wrapper flex flex-row position-absolute">
+                <section className="three shadow position-relative z-front">
+                    
+                </section>
+                <section className="seven position-relative z-back">
+                    <Maps></Maps>
+                </section>
+            </main>
+            )
+    }
 };
 
 export default App;
