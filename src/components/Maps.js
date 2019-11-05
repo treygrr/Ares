7d1c65ed84d3131fabf90a3380ea5b19c1786c20
trey <trey@lavidadev.com>
2019-11-05 15:03:57 -0500

    Update and added map component functionality

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import React, { Component } from "react"
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 
-function MapSet() {
-    return(
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
-        />
-    )
-};
+import { connect } from 'react-redux';
+import { fetch, currentMarker } from '../actions';
+import { compose } from "recompose";
 
-const WrappedMap = withScriptjs(withGoogleMap(MapSet));
+import MarkerCollection from './MarkerCollection'
 
-class Maps extends React.Component {
 
-    render() {
-        return(
-            <div style= {{ width: "100%", height: "100vh" }}>
-                <WrappedMap 
-                    googleMapURL= {`https://maps.googleapis.com/maps/api/js?key=AIzaSyCbq5Ppo2m86ozXkXCgHlLeZot9S8yxO44&v=3.exp&libraries=geometry,drawing,places`}
-                    loadingElement={<div style={{ height: "100%" }}/>}
-                    containerElement={<div style={{ height: "100%" }}/>}
-                    mapElement={<div style={{ height: "100%" }}/>}
-                />
-            </div>
-        )
+
+class Maps extends Component {
+  getCords(data) {
+    if (data !== null) {
+      return { lat: data.position.latitude, lng: data.position.longitude };
     }
-};
+    
+    console.log(data);
+    return { lat: 33.5, lng: -118.417931 };
+  }
 
-export default Maps;
+  render() {
+   
+
+    const Map = compose(withScriptjs, withGoogleMap)(val => {
+      return (
+        <GoogleMap
+          defaultZoom={13}
+          center={this.getCords(this.props.selectedMarker)}
+        >
+          <MarkerCollection data={this.props.posts}></MarkerCollection>
+        </GoogleMap>
+      )
+    });
+    return (
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+    return { 
+        posts: state.posts,
+        selectedMarker: state.currentMarker
+    };
+}
+
+export default connect(mapStateToProps, { fetch, currentMarker })(Maps);
